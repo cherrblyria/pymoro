dec7dbadae07fad01012e493cc61cf24693c7466
cherrblyria <furytorch.1861@gmail.com>
2026-04-07 23:27:11 +0700

    🚀 init commit!

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 210,
+    height: 230,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: false,
+    webPreferences: {
+      contextIsolation: true,
+    },
+  });
+
+  win.loadFile("src/index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
